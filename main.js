8bc9b11c7ba53f6ed06f981e5ad11b149a913990
OuachouX7 <155732037+OuachouX7@users.noreply.github.com>
2024-08-07 00:19:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let txt = document.getElementById('txt');
 let txtval = txt.value;
 let sub = document.getElementById('sub');
+let tasks = document.getElementById('tasks');
 
 sub.addEventListener('click',submit);
 
@@ -9,10 +10,12 @@ function submit(event){
 
     let txt = document.getElementById('txt');
     let txtval = txt.value;
-    let btn = document.createElement('button');
-    btn.className = 'delete';
-    btn.textContent = 'X';
-    btn.style.backgroundColor = '#FFF';
+    let span = document.createElement('span');
+    span.className = "delete";
+    span.innerHTML = "<i class='fa-solid fa-trash'></i>";
+    let done = document.createElement('span');
+    done.className = 'done';
+    done.innerHTML = "<i class='fa-regular fa-square-check'></i>";
     let div = document.createElement('div');
     let para = document.createElement('p');
 
@@ -21,22 +24,43 @@ function submit(event){
     
     para.style.textAlign = 'center';
     
-    btn.addEventListener('click',Deleting);
+    span.addEventListener('click',Deleting);
     
     function Deleting(){
-        btn.parentNode.remove();
+        span.parentNode.remove();
         
     }
+
+    done.addEventListener('click',WellDone);
+
+    function WellDone(){
+        let lt = done.nextElementSibling; 
+        lt.classList.toggle('line-through');
+        if (lt.classList.contains('line-through')) {
+            done.classList.add('completed');
+        }else{
+            done.classList.remove('completed');
+        }
+    }
     
+
     div.style.backgroundColor = "#EEE";
 
     div.style.display = 'flex';
-    div.style.justifyContent = 'space-between';
+    div.style.justifyContent = 'space-evenly';
+    div.style.alignItems = 'center';
+    div.style.margin = '15px 0';
 
+    div.appendChild(done);
     div.appendChild(para);
-    div.appendChild(btn);
+    div.appendChild(span);
+
+    div.className = 'task-container';
+
+    tasks.appendChild(div);
 
     
+    
 
-    document.body.appendChild(div);
+//    bouh gay
 }
